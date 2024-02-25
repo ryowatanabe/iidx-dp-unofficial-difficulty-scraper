f956cf1f8e5e4a7480543d834fd5a7e72ffda140
Ryo Watanabe <watanabe.ryo@gmail.com>
2024-02-25 20:25:17 +0900

    changed sort

--- export_csv.sh
+++ export_csv.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-sqlite3 -header -csv ./data/data.db "select * from ereternet_difficulty" > ./data/ereternet_difficulty.csv
-sqlite3 -header -csv ./data/data.db "select * from unofficial_difficulty" > ./data/unofficial_difficulty.csv
+sqlite3 -header -csv ./data/data.db "select * from ereternet_difficulty order by song_id" > ./data/ereternet_difficulty.csv
+sqlite3 -header -csv ./data/data.db "select * from unofficial_difficulty order by song_id" > ./data/unofficial_difficulty.csv
