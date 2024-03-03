a9067f038c1cacefa28624214fa0ac188ffeb04b
Ryo Watanabe <watanabe.ryo@gmail.com>
2024-03-03 21:09:05 +0900

    added song_id into csv output

--- export_csv.sh
+++ export_csv.sh
@@ -2,4 +2,4 @@
 
 sqlite3 -header -csv ./data/data.db "select * from unofficial_difficulty order by song_id" > ./data/unofficial_difficulty.csv
 sqlite3 -header -csv ./data/data.db "select * from ereternet_difficulty order by song_id" > ./data/ereternet_difficulty.csv
-sqlite3 -header -csv ./data/data.db "select name, difficulty, unofficial_diff, ec_diff, hc_diff, exh_diff from ereternet_difficulty order by song_id" > ./data/ereternet_difficulty_diffonly.csv
+sqlite3 -header -csv ./data/data.db "select song_id, name, difficulty, unofficial_diff, ec_diff, hc_diff, exh_diff from ereternet_difficulty order by song_id" > ./data/ereternet_difficulty_diffonly.csv
